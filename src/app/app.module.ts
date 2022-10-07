c2db0a96fdea86d5d9d102f716a1ab3fa83d1d93
Loovelllyy <sedykh.k.n@gmail.com>
2022-10-07 18:26:09 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './сomponents/main-page/main-page.component';
+import { CurrencyCardComponent } from './сomponents/currency-card/currency-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatSliderModule} from "@angular/material/slider";
+import {MaterialModule} from "../../material.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    CurrencyCardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSliderModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
