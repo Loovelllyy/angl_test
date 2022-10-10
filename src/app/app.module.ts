e0ffa2b628165f755eaff115cf2720d4c521916b
Loovelllyy <sedykh.k.n@gmail.com>
2022-10-10 18:43:28 +0300

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './сomponents/main-page/main-page.component';
-import { CurrencyCardComponent } from './сomponents/currency-card/currency-card.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatSliderModule} from "@angular/material/slider";
 import {MaterialModule} from "../../material.module";
+import { HeaderComponent } from './components/header/header.component';
+import { UserHelloComponent } from './components/header/user-hello/user-hello.component';
+import { SearchComponentComponent } from './components/header/search-component/search-component.component';
+import {FormsModule} from "@angular/forms";
+import { ShortProfileComponent } from './components/header/short-profile/short-profile.component';
+import { CoinCardComponent } from './components/coin-card/coin-card.component';
+import {HttpClientModule} from "@angular/common/http";
+import { MarketComponentComponent } from './components/market-component/market-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    CurrencyCardComponent,
+    HeaderComponent,
+    UserHelloComponent,
+    SearchComponentComponent,
+    ShortProfileComponent,
+    CoinCardComponent,
+    MarketComponentComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,7 +34,9 @@ import {MaterialModule} from "../../material.module";
     BrowserAnimationsModule,
     MatButtonModule,
     MatSliderModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
